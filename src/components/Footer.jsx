74da852eb866e11bb4b5c26123ce49cac8cfbc8f
Suraj <surajbahadur58@gmail.com>
2023-03-07 22:38:02 +0530

    landing page fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import logo from '../assets/food logo.png'
 
 const Footer = () => {
   return (
     <div>
         <div className='bg-black flex-col text-white  text-center'>
-            <h1 className='py-1'>Dessert.Bar.Kitchen</h1>
+        <img src={logo} alt="logo" height='50px' width='50px' className=' mx-auto'/>
+            <h1 className='py-1 text-lg font-semibold'>Dessert.Bar.Kitchen</h1>
             <h1 className='py-1'>+040 2355 7261</h1>
             <h1 className='py-1'>savory@qodeinteractive.com</h1>
-            <h1 className='py-1'>Plot No.8-2, 293/82/A/161, Rd Number 13, Jubilee Hills, Hyderabad, Telangana 500033</h1>
-            <h1 className='py-1'>Social Media</h1>
+            <h1 className='py-1 px-[500px]'>Plot No.8-2, 293/82/A/161, Rd Number 13, Jubilee Hills, Hyderabad, Telangana 500033</h1>
+            <h1 className='py-1'>Social Media Icons</h1>
             <h1 className='py-1'>TECHARION 2023</h1>
 
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,20 @@ const Hero = () => {
   return (
     <>
     
-    <div className='h-[709px] py-[250px] grid grid-cols-2  text-white bg-gray-900'  >
+    <div className='h-[680px] py-[250px] grid grid-cols-2  text-white bg-gray-900' style={{ 
+      backgroundImage: `url(${Pic1})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize:"contain",
+      
+    }}  >
+      {/* <img src={Pic1} alt="" /> */}
 
     <div> </div>
     <div >
-      {/* <img src={Pic1} alt="" /> */}
     <h1 className=' text-4xl my-4'>DESSERT | BAR | KITCHEN</h1>
         <h1 className='text-2xl'>The Barn is a global food experience and our dessert bar is legendary!</h1>
 
-        <button className='bg-[#C5B5A6] px-2 py-1 my-4 rounded-2xl'>Contact Us</button>
+        <button className='bg-[#C5B5A6] text-[#000000] px-2 py-1 my-4 rounded-2xl'>Contact Us</button>
         
 
     </div>
